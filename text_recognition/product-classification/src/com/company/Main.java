0a73f67cc842c11ddb335566c227b869d1861cce
danhaus <danhaus95@gmail.com>
2018-01-28 01:13:30 +0000

    barcode scan

---
+++ text_recognition/product-classification/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+// This sample uses the Apache HTTP client library(org.apache.httpcomponents:httpclient:4.2.4)
+// and the org.json library (org.json:json:20170516).
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Ocr myOcr = new Ocr();
+        Ocr.runOcr();
+    }
+}
